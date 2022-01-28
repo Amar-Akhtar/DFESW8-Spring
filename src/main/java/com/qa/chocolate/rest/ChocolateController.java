46d7f5adf0752a71d74c13104a23e2c08effb288
lifebeginsat30 <amar.akhtar@icloud.com>
2022-01-28 15:15:51 +0000

    response entity complete

--- src/main/java/com/qa/chocolate/rest/ChocolateController.java
+++ src/main/java/com/qa/chocolate/rest/ChocolateController.java
@@ -3,10 +3,13 @@ package com.qa.chocolate.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +20,22 @@ public class ChocolateController {
 
 	private List<Chocolate> choco = new ArrayList<>();
 	
+	//Response entity -> allows us to configure the status code response 
+	
+	
 	//CRUD
 	
 	//Create - Post
+	//Response entity -> response to return instead of 200: 201 (created)
 	@PostMapping("/createChoco")
-	public void createChoco(@RequestBody Chocolate c) {
+	public ResponseEntity<Chocolate> createChoco(@RequestBody Chocolate c) {
 		//service -> create
 		this.choco.add(c);
+		
+		// get the record we just created 
+		Chocolate newChoco = this.choco.get(this.choco.size()-1);
+														//Enum because its constant
+		return new ResponseEntity<Chocolate>(newChoco, HttpStatus.CREATED);
 	}
 	
 	//insert into chocolate(name, brand, ...) values("dairy milk", "cadbury",...)
@@ -42,6 +54,18 @@ public class ChocolateController {
 	
 	
 	//Update - Put/Patch (PUT)
+	//create, read by id
+	@PutMapping("/updateChoco/{id}")
+	public Chocolate updateChoco(@PathVariable int id, @RequestBody Chocolate ch) {
+		//remove existing choco with id
+		this.choco.remove(id);
+		//add a new choco in its place 
+		this.choco.add(id, ch);
+		//return updated choco
+		return this.choco.get(id);
+		
+	}
+	
 	
 	//Delete - Delete
 	@DeleteMapping("/removeChoco/{id}")
